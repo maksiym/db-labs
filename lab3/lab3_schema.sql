5f372dc7590aca3e25de9ccb5d14ea9d8767f256
maks morochenets <maksmorroo@gmail.com>
2022-10-05 23:45:01 +0300

    added indexes & updated script

--- lab3/lab3_schema.sql
+++ lab3/lab3_schema.sql
@@ -1,175 +1,144 @@
--- MySQL Workbench Forward Engineering
-
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
-
 -- -----------------------------------------------------
--- Schema lab1_schema
+-- Schema lab3_schema
 -- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `lab3_schema` DEFAULT CHARACTER SET utf8 ;
+USE `lab3_schema` ;
+
 
 -- -----------------------------------------------------
--- Schema lab1_schema
+-- Table `lab3_schema`.`office`
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `lab1_schema` DEFAULT CHARACTER SET utf8 ;
-USE `lab1_schema` ;
-
--- -----------------------------------------------------
--- Table `lab1_schema`.`office`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `lab1_schema`.`office` (
+DROP TABLE IF EXISTS `lab3_schema`.`office`;
+CREATE TABLE IF NOT EXISTS `lab3_schema`.`office` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `number` INT NOT NULL,
   `city` VARCHAR(25) NOT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
+  PRIMARY KEY (`id`));
 
 
 -- -----------------------------------------------------
--- Table `lab1_schema`.`room`
+-- Table `lab3_schema`.`room`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `lab1_schema`.`room` (
+DROP TABLE IF EXISTS `lab3_schema`.`room`;
+CREATE TABLE IF NOT EXISTS `lab3_schema`.`room` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `number` INT NOT NULL,
   `office_id` INT NOT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_room_office1_idx` (`office_id` ASC) VISIBLE,
-  CONSTRAINT `fk_room_office1`
+  CONSTRAINT `fk_room_office_id`
     FOREIGN KEY (`office_id`)
     REFERENCES `lab1_schema`.`office` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+);
 
 
 -- -----------------------------------------------------
 -- Table `lab1_schema`.`workplace`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `lab1_schema`.`workplace` (
+DROP TABLE IF EXISTS `lab3_schema`.`workplace`;
+CREATE TABLE IF NOT EXISTS `lab3_schema`.`workplace` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `number` INT NOT NULL,
   `room_id` INT NOT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_workplace_room1_idx` (`room_id` ASC) VISIBLE,
-  CONSTRAINT `fk_workplace_room1`
+  CONSTRAINT `fk_workplace_room_id`
     FOREIGN KEY (`room_id`)
     REFERENCES `lab1_schema`.`room` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+);
 
 
 -- -----------------------------------------------------
--- Table `lab1_schema`.`equipment_location`
+-- Table `lab3_schema`.`equipment_location`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `lab1_schema`.`equipment_location` (
+DROP TABLE IF EXISTS `lab3_schema`.`equipment_location`;
+CREATE TABLE IF NOT EXISTS `lab3_schema`.`equipment_location` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `workplace_id` INT NOT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_equipment_location_workplace1_idx` (`workplace_id` ASC) VISIBLE,
-  CONSTRAINT `fk_equipment_location_workplace1`
+  CONSTRAINT `fk_equipment_location_workplace_id`
     FOREIGN KEY (`workplace_id`)
     REFERENCES `lab1_schema`.`workplace` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+);
 
 
 -- -----------------------------------------------------
--- Table `lab1_schema`.`issue`
+-- Table `lab3_schema`.`issue`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `lab1_schema`.`issue` (
+DROP TABLE IF EXISTS `lab3_schema`.`issue`;
+CREATE TABLE IF NOT EXISTS `lab3_schema`.`issue` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `description` VARCHAR(10000) NULL,
   `equipment_location_id` INT NOT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_issue_equipment_location1_idx` (`equipment_location_id` ASC) VISIBLE,
-  CONSTRAINT `fk_issue_equipment_location1`
+  CONSTRAINT `fk_issue_equipment_location_id`
     FOREIGN KEY (`equipment_location_id`)
     REFERENCES `lab1_schema`.`equipment_location` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+);
 
 
 -- -----------------------------------------------------
--- Table `lab1_schema`.`initiator`
+-- Table `lab3_schema`.`initiator`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `lab1_schema`.`initiator` (
+DROP TABLE IF EXISTS `lab3_schema`.`initiator`;
+CREATE TABLE IF NOT EXISTS `lab3_schema`.`initiator` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(25) NOT NULL,
   `surname` VARCHAR(25) NOT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
+  PRIMARY KEY (`id`));
 
 
 -- -----------------------------------------------------
--- Table `lab1_schema`.`application`
+-- Table `lab3_schema`.`application`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `lab1_schema`.`application` (
+DROP TABLE IF EXISTS `lab3_schema`.`application`;
+CREATE TABLE IF NOT EXISTS `lab3_schema`.`application` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `date` DATETIME NOT NULL,
   `status` VARCHAR(25) NULL,
   `issue_id` INT NOT NULL,
   `initiator_id` INT NOT NULL,
   PRIMARY KEY (`id`, `issue_id`, `initiator_id`),
-  INDEX `fk_application_issue1_idx` (`issue_id` ASC) VISIBLE,
-  INDEX `fk_application_initiator1_idx` (`initiator_id` ASC) VISIBLE,
-  CONSTRAINT `fk_application_issue1`
+  CONSTRAINT `fk_application_issue_id`
     FOREIGN KEY (`issue_id`)
-    REFERENCES `lab1_schema`.`issue` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_application_initiator1`
+    REFERENCES `lab1_schema`.`issue` (`id`),
+  CONSTRAINT `fk_application_initiator_id`
     FOREIGN KEY (`initiator_id`)
     REFERENCES `lab1_schema`.`initiator` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+);
+
 
 
 -- -----------------------------------------------------
--- Table `lab1_schema`.`respondent`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `lab1_schema`.`respondent` (
+-- Table `lab3_schema`.`respondent`
+-- ----------------------------------3-------------------
+DROP TABLE IF EXISTS `lab3_schema`.`respondent`;
+CREATE TABLE IF NOT EXISTS `lab3_schema`.`respondent` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(25) NOT NULL,
   `surname` VARCHAR(25) NOT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
+  PRIMARY KEY (`id`));
 
 
 -- -----------------------------------------------------
--- Table `lab1_schema`.`application_respondent`
+-- Table `lab3_schema`.`application_respondent`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `lab1_schema`.`application_respondent` (
+DROP TABLE IF EXISTS `lab3_schema`.`application_respondent`;
+CREATE TABLE IF NOT EXISTS `lab3_schema`.`application_respondent` (
   `application_id` INT NOT NULL,
   `respondent_id` INT NOT NULL,
   PRIMARY KEY (`application_id`, `respondent_id`),
-  INDEX `fk_application_has_respondent_respondent1_idx` (`respondent_id` ASC) VISIBLE,
-  INDEX `fk_application_has_respondent_application1_idx` (`application_id` ASC) VISIBLE,
-  CONSTRAINT `fk_application_has_respondent_application1`
+  CONSTRAINT `fk_application_respondent_application_id`
     FOREIGN KEY (`application_id`)
-    REFERENCES `lab1_schema`.`application` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_application_has_respondent_respondent1`
+    REFERENCES `lab1_schema`.`application` (`id`),
+  CONSTRAINT `fk_application_respondent_respondent_id`
     FOREIGN KEY (`respondent_id`)
     REFERENCES `lab1_schema`.`respondent` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+);
 
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
-
-USE lab1_schema;
-
-
--- Office
-INSERT INTO office(number, city) VALUES
+-- -----------------------------------------------------
+-- Table `lab3_schema`.`office`
+-- -----------------------------------------------------
+INSERT INTO `office`(`number`, `city`) VALUES
 (1, 'Rivne'),
 (2, 'Kyiv'),
 (3, 'Lviv'),
@@ -181,8 +150,11 @@ INSERT INTO office(number, city) VALUES
 (9, 'Odesa'),
 (10, 'Kherson');
 
--- Room
-INSERT INTO room(number, office_id) VALUES
+
+-- -----------------------------------------------------
+-- Table `lab3_schema`.`room`
+-- -----------------------------------------------------
+INSERT INTO `room`(`number`, `office_id`) VALUES
 (501, 1),
 (503, 2),
 (503, 3),
@@ -194,8 +166,11 @@ INSERT INTO room(number, office_id) VALUES
 (307, 7),
 (907, 8);
 
--- Workplace
-INSERT INTO workplace(number, room_id) VALUES
+
+-- -----------------------------------------------------
+-- Table `lab3_schema`.`workplace`
+-- -----------------------------------------------------
+INSERT INTO `workplace`(`number`, `room_id`) VALUES
 (11, 1),
 (12, 4),
 (13, 3),
@@ -207,8 +182,11 @@ INSERT INTO workplace(number, room_id) VALUES
 (17, 7),
 (18, 8);
 
--- Equipment location
-INSERT INTO equipment_location(workplace_id) VALUES
+
+-- -----------------------------------------------------
+-- Table `lab3_schema`.`equipment_location`
+-- -----------------------------------------------------
+INSERT INTO `equipment_location`(`workplace_id`) VALUES
 (1),
 (2),
 (3),
@@ -220,8 +198,11 @@ INSERT INTO equipment_location(workplace_id) VALUES
 (9),
 (10);
 
--- Initiator 
-INSERT INTO initiator(name, surname) VALUES
+
+-- -----------------------------------------------------
+-- Table `lab3_schema`.`initiator`
+-- -----------------------------------------------------
+INSERT INTO `initiator`(`name`, `surname`) VALUES
 ('Tolik', 'Sharii'),
 ('Jay', 'A'),
 ('Jay', 'B'),
@@ -233,8 +214,11 @@ INSERT INTO initiator(name, surname) VALUES
 ('Zinoviy', 'Mychuda'),
 ('Zynoviy', 'Nytrebych');
 
--- Respondent
-INSERT INTO respondent(name, surname) VALUES
+
+-- -----------------------------------------------------
+-- Table `lab3_schema`.`respondent`
+-- -----------------------------------------------------
+INSERT INTO `respondent`(`name`, `surname`) VALUES
 ('Eminem', 'Bylen'),
 ('Jack', 'Gorobets'),
 ('Anatolii', 'Petrovych'),
@@ -246,8 +230,11 @@ INSERT INTO respondent(name, surname) VALUES
 ('Svyat', 'Lord'),
 ('Svyat', 'Must-have');
 
--- Issue
-INSERT INTO issue(description, equipment_location_id) VALUES
+
+-- -----------------------------------------------------
+-- Table `lab3_schema`.`issue`
+-- -----------------------------------------------------
+INSERT INTO `issue`(`description`, `equipment_location_id`) VALUES
 ('SW-failure', 1),
 ('SW-failure', 2),
 ('SW-failure', 3),
@@ -259,8 +246,11 @@ INSERT INTO issue(description, equipment_location_id) VALUES
 ('PC-failure', 9),
 ('PC-failure', 10);
 
--- Application
-INSERT INTO application(date, status, issue_id, initiator_id) VALUES
+
+-- -----------------------------------------------------
+-- Table `lab3_schema`.`application`
+-- -----------------------------------------------------
+INSERT INTO `application`(`date`, `status`, `issue_id`, `initiator_id`) VALUES
 ('2022-09-13', 'NEW', 1, 10),
 ('2022-09-14', 'NEW', 2, 1),
 ('2022-09-15', 'NEW', 3, 6),
@@ -272,8 +262,11 @@ INSERT INTO application(date, status, issue_id, initiator_id) VALUES
 ('2022-09-23', 'NEW', 9, 9),
 ('2022-09-30', 'NEW', 10, 5);
 
--- Application respondent
-INSERT INTO application_respondent(application_id, respondent_id) VALUES
+
+-- -----------------------------------------------------
+-- Table `lab3_schema`.`application_respondent`
+-- -----------------------------------------------------
+INSERT INTO `application_respondent`(`application_id`, `respondent_id`) VALUES
 (1, 1),
 (1, 2),
 (1, 3),
@@ -284,3 +277,7 @@ INSERT INTO application_respondent(application_id, respondent_id) VALUES
 (6, 7),
 (7, 8),
 (8, 9);
+
+
+CREATE INDEX `initiator_surname_name_idx` ON `initiator`(`surname`, `name`);
+CREATE INDEX `respondent_surname_name_idx` ON `respondent`(`surname`, `name`);
